8d381fae0b5fe78d7f5c72f5a5ba6134ac10ea4b
Jia Jie Liang <dongfangjixuezhong@gmail.com>
2025-06-29 03:37:35 +0800

    Footer Web Component 修

--- js/components/indexFooter.js
+++ js/components/indexFooter.js
@@ -21,8 +21,25 @@ class IndexFooter extends HTMLElement {
             loadTemplate(`${basePath}/pages/components/indexFooter.html`),
             loadTemplate(`${basePath}/css/components/indexFooter.css`)
         ]);
-    
-    this.shadowRoot.innerHTML = `<style>${css}</style>${html}`;
+        const fixedHtml = html;
+        // 建立 Font Awesome 的 link 元素
+        const fontAwesomeLink = document.createElement('link');
+        fontAwesomeLink.rel = 'stylesheet';
+        fontAwesomeLink.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css';
+        // 建立 <style>
+        const styleEl = document.createElement('style');
+        styleEl.textContent = css
+        // 包裝 HTML 為 element
+        const wrapper = document.createElement('div');
+        wrapper.innerHTML = fixedHtml;
+        
+        this.shadowRoot.appendChild(fontAwesomeLink);
+        this.shadowRoot.appendChild(styleEl);
+        this.shadowRoot.appendChild(wrapper);
+
+        // #Home 
+        const homeLink = this.shadowRoot.getElementById('home-link');
+        homeLink.href = `${basePath}/`;
     }
 }
 

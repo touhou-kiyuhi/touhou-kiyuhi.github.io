754d787afd14d087f2e534d214042aa5ba7bfa6e
Jia Jie Liang <dongfangjixuezhong@gmail.com>
2025-06-29 20:58:48 +0800

    版面響應，添加處理手機部分

--- js/components/homeHeader.js
+++ js/components/homeHeader.js
@@ -23,11 +23,38 @@ class HomeHeader extends HTMLElement {
         ]);
         // 替換 HTML 內的 logo 路徑
         const fixedHtml = html.replace(/src="\.?\/?images\/logo\.png"/g, `src="${basePath}/images/logo.png"`);
-        this.shadowRoot.innerHTML = `<style>${css}</style>${fixedHtml}`;
+
+        // 建立 Font Awesome 的 link 元素
+        const fontAwesomeLink = document.createElement('link');
+        fontAwesomeLink.rel = 'stylesheet';
+        fontAwesomeLink.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css';
         
+        // 建立 <style>
+        const styleEl = document.createElement('style');
+        styleEl.textContent = css;
+        
+        // 包裝 HTML 為 element
+        const wrapper = document.createElement('div');
+        wrapper.innerHTML = fixedHtml;
+        
+        // 加入所有內容進 shadow DOM
+        this.shadowRoot.appendChild(fontAwesomeLink);
+        this.shadowRoot.appendChild(styleEl);
+        this.shadowRoot.appendChild(wrapper);
+
         // #Home 
         const homeLink = this.shadowRoot.getElementById('home-link');
         homeLink.href = `${basePath}/pages/home.html`;
+
+        // ✅ 響應式 menu toggle
+        const mobileMenu = this.shadowRoot.querySelector('.mobile-menu');
+        const navLinks = this.shadowRoot.querySelector('#myLinks');
+
+        if (mobileMenu && navLinks) {
+            mobileMenu.addEventListener('click', () => {
+                navLinks.classList.toggle('show');
+            });
+        }
     }
 }
 

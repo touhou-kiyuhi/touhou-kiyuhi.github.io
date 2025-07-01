01780ef6ff66fc3fb9e528e80014cdcf0a48045f
touhou-kiyuhi <52646651+touhou-kiyuhi@users.noreply.github.com>
2025-07-01 03:39:14 +0000

    添加 description ，先隱藏後出現

--- js/about_script.js
+++ js/about_script.js
@@ -11,14 +11,15 @@ document.addEventListener('DOMContentLoaded', function() {
     // 確保 custom element 加載完成
     customElements.whenDefined('user-card').then(() => {
 		const cardWrapper = document.querySelector('.user-card-wrapper');
+        const description = document.querySelector('.description-text');
 
 		// 只有當螢幕寬度大於 1225px 時才執行滑動
 		if (window.innerWidth > 1000) {
 			setTimeout(() => {
-                const parent = cardWrapper.parentElement;
-                const moveDistance = parent.offsetWidth * 0.3; // 父容器寬度的 30%
-                cardWrapper.style.transform = `translateX(${moveDistance}px)`; // 使用 JS 設定 transform
-				cardWrapper.classList.add('slide-right');
+                cardWrapper.classList.add('move-to-4');
+                setTimeout(() => {
+                    description.classList.add('show');
+                }, 1000);
             // 等 3 秒再滑動
 			}, 3000);
 		}
